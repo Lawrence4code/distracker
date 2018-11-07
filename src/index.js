a5649726b094965c63b1c9f63d2693ffa3a003fe
Lawrence <Lawrence4code@gmail.com>
2018-11-08 03:59:50 +0530

    Created form functionality and integrated firebase for database

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
-import './styles.scss'; // sass file import
+import database from './firebase';
+import './styles.scss';
 
-console.log('index.js initiated.');
+// dom elements
+const btns = document.querySelectorAll('button');
+const form = document.querySelector('form');
+const formAct = document.querySelector('form span');
+const input = document.querySelector('input');
+const error = document.querySelector('.error');
+
+let activity = 'cycling';
+
+btns.forEach(btn => {
+  btn.addEventListener('click', e => {
+    activity = btn.dataset.activity;
+    btns.forEach(btn => {
+      btn.classList.remove('active');
+      e.target.classList.add('active');
+      input.setAttribute('id', activity);
+
+      formAct.textContent = activity;
+    });
+  });
+});
+
+// form submit
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const distance = parseInt(input.value);
+
+  if (distance) {
+    database
+      .collection('activities')
+      .add({ distance, activity, date: new Date().toString() })
+      .then(() => {
+        (error.textContent = ''), (input.value = '');
+      });
+  } else {
+    error.textContent = 'Please enter a valid input (Distance in meters)';
+  }
+});
+
+console.log('******* Issue to Fix *******');
+console.log(
+  '1) Slight delay in res from firebase results in input data staying in the input field and multiple enter results in duplicate records, fix it!'
+);
+console.log(
+  '2) Optional, add animation when button (done) and text are clicked make it smooth'
+);
